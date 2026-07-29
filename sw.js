667b2dc4e617f992ccfa9cbb20b4a40f9cd06834
KizzyMoon <chelseacaitline@gmail.com>
2026-07-29 22:18:02 +0100

    Animate preset artwork lighting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE='lofi-room-v21';
+const CACHE='lofi-room-v22';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.svg','presets.json','assets/awake.jpg','assets/busy.jpg','assets/away.jpg','assets/ems.jpg','assets/training.jpg','assets/chilling.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
